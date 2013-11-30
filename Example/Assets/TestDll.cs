b405cf79a462c48c93d7235b1ff4b0976428de51
nobnak <nobu.nakata@gmail.com>
2013-11-30 16:07:25 +0900

    キャニー法のテスト

---
+++ Example/Assets/TestDll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+using System.IO;
+
+public class TestDll : MonoBehaviour {
+	public string srcImagePath;
+	public string dstImagePath;
+
+	// Use this for initialization
+	void Start () {
+		using (var src = new IplImage(BuildPath(srcImagePath), LoadMode.GrayScale))
+		using (var dst = new IplImage(src.Size, BitDepth.U8, 1)) {
+			src.Canny(dst, 50, 200);
+			dst.SaveImage(BuildPath(dstImagePath));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public string BuildPath(string filename) {
+		return Path.Combine(Application.streamingAssetsPath, filename);
+	}
+}
