51ab36fb0cbbfb78f6dd6f3cb336a52bdf64accc
nnakata <nakata@team-lab.com>
2013-12-03 14:03:29 +0900

    Macでの動作確認

--- Example/Assets/TestDll.cs
+++ Example/Assets/TestDll.cs
@@ -9,25 +9,31 @@ public class TestDll : MonoBehaviour {
 
 	private Texture2D _tex;
 	private CvCapture _cap;
+	private IplImage _eigImage;
 	private IplImage _tmpImage;
 
 	// Use this for initialization
 	void Start () {
 		_tex = new Texture2D(0, 0);
 		renderer.sharedMaterial.mainTexture = _tex;
-		//_cap = new CvCapture(0);
+		_cap = new CvCapture(0);
 
-		Test01();
+		//Test01b();
+		//Test02 ();
 	}
 
 	void Update() {
-		//Test02 ();
+		Test01b();
 	}
 
 	void OnDestroy() {
 		if (_cap != null) { 
 			_cap.Dispose();
 			_cap = null;
+		}
+		if (_eigImage != null) {
+			_eigImage.Dispose();
+			_eigImage = null;
 		}
 		if (_tmpImage != null) {
 			_tmpImage.Dispose();
@@ -44,13 +50,27 @@ public class TestDll : MonoBehaviour {
 			ShowImage(dst);
 		}
 	}
+	
+	void Test01b() {
+		using (var src = _cap.QueryFrame())
+		using (var dst = new IplImage(src.Size, BitDepth.U8, 1)) {
+			src.Canny(dst, 50, 200);
+			ShowImage(dst);
+		}
+	}
 
 	void Test02() {
 		using (var image = _cap.QueryFrame()) {
+			if (_eigImage == null)
+				_eigImage = new IplImage(image.Size, BitDepth.F32, 1);
 			if (_tmpImage == null)
-				_tmpImage = new IplImage(image.Size, image.Depth, 1);
-			image.Canny(_tmpImage, 50, 200);
-			ShowImage(_tmpImage);
+				_tmpImage = new IplImage(image.Size, BitDepth.F32, 1);
+			CvPoint2D32f[] corners;
+			int nCorners = 0;
+			var qualityLevel = 0.01;
+			var minDistance = 0.01;
+			Cv.GoodFeaturesToTrack(image, _eigImage, _tmpImage, out corners, ref nCorners, qualityLevel, minDistance);
+
 		}
 	}
 
